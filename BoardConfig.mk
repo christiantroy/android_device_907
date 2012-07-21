0eaede55522610baeef912ecf198961ae121e326
Alan Marchesan <justcavalli@gmail.com>
2012-07-21 08:40:25 +0200

    temporary workaround for compilation issue

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,10 +49,10 @@ BOARD_USE_SKIA_LCDTEXT := true
 ENABLE_WEBGL := true
 
 #Recovery Stuff
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/softwinner/907/recovery_keys.c
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/softwinner/907/recovery_keys.c
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
-TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
+#TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
 
 #Misc stuff
 TARGET_USE_CUSTOM_LUN_FILE_PATH = "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
